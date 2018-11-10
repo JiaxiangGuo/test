afa8ce3b6e9fa167151414871847976ac2818948
Guojiaxiang <guojiaxiang1993@163.com>
2018-11-10 20:27:41 +0800

    oo

--- http/app.js
+++ http/app.js
@@ -1,8 +1,8 @@
 var http = require('http')
 http
     .createServer(function(req, res){
-        res.writeHead(200, {'Content-Type':'text/plain'})
-        res.write('hello Node.js')
-        res.end()
+        res.writeHead(200, {'Content-Type':'text/plain'});
+        res.write('hello Node.js========');
+        res.end();
     })
-    .listen(80)
+    .listen(80);
