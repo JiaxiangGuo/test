016c379d0fcebc3eeb0de7da26d8a600aff81cbc
Guojiaxiang <guojiaxiang1993@163.com>
2018-11-10 22:56:14 +0800

    oodd

--- http/app.js
+++ http/app.js
@@ -1,8 +1,9 @@
 var http = require('http')
 http
     .createServer(function(req, res){
-        res.writeHead(200, {'Content-Type':'text/plain'});
-        res.write('老婆我爱你！！！', "utf-8");
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write('<head><meta charset="utf-8"/></head>');
+        res.write('<h1><font color="red">老婆我爱你！！！</font></h1>');
         res.end();
     })
     .listen(80);
